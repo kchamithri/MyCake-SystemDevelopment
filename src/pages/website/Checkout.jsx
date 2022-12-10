696a3581ca8ceb027d2524a489c8d36c48708357
kchamithri <gangodaw-im18025@stu.kln.ac.lk>
2022-12-11 02:03:32 +0530

    checkout connected with backend

--- src/pages/website/Checkout.jsx
+++ src/pages/website/Checkout.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { Button, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
+import swal from "@sweetalert/with-react";
 
 const Checkout = () => {
   const [validated, setValidated] = useState(false);
+  const today = new Date();
   const [order, setOrder] = useState({
     firstName: "",
     lastName: "",
@@ -15,17 +17,69 @@ const Checkout = () => {
     senderName: "",
     senderContact: "",
     senderEmail: "",
+    orderPlacedDate: today,
   });
 
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+  const handleInput = (event) => {
+    let name = event.target.name;
+    let value = event.target.value;
 
-    setValidated(true);
+    setOrder({ ...order, [name]: value });
   };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const {
+      firstName,
+      lastName,
+      address,
+      city,
+      contact,
+      deliverDate,
+      deliverTime,
+      message,
+      senderName,
+      senderContact,
+      senderEmail,
+      orderPlacedDate,
+    } = order;
+
+    try {
+      const res = await fetch("/checkout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          firstName,
+          lastName,
+          address,
+          city,
+          contact,
+          deliverDate,
+          deliverTime,
+          message,
+          senderName,
+          senderContact,
+          senderEmail,
+          orderPlacedDate,
+        }),
+      });
+      if (res.status === 400 || !res) {
+        window.alert("Invalid Credentials");
+      } else {
+        swal("Success", "Successfully Added", "success", {
+          button: false,
+          timer: 1500,
+        });
+        console.log(event.target);
+      }
+    } catch (error) {
+      console.log("ERROR IS", error);
+    }
+  };
+
   return (
     <Container className="mt-3 border border-darker ">
       <h4 style={{ fontFamily: "monospace", fontSize: "20px" }}>
@@ -41,14 +95,26 @@ const Checkout = () => {
         <Row className="mb-3">
           <Form.Group as={Col} md="6" controlId="validationCustom01">
             <Form.Label>First name</Form.Label>
-            <Form.Control required type="text" placeholder="First name" />
+            <Form.Control
+              required
+              type="text"
+              placeholder="First name"
+              name="firstName"
+              onChange={handleInput}
+            />
             <Form.Control.Feedback type="invalid">
               Please provide your First Name.
             </Form.Control.Feedback>
           </Form.Group>
           <Form.Group as={Col} md="6" controlId="validationCustom02">
             <Form.Label>Last name</Form.Label>
-            <Form.Control required type="text" placeholder="Last name" />
+            <Form.Control
+              required
+              type="text"
+              placeholder="Last name"
+              name="lastName"
+              onChange={handleInput}
+            />
             <Form.Control.Feedback type="invalid">
               Please provide a Last Name.
             </Form.Control.Feedback>
@@ -57,7 +123,13 @@ const Checkout = () => {
         <Row className="mb-3">
           <Form.Group as={Col} md="12" controlId="validationCustom03">
             <Form.Label>Address</Form.Label>
-            <Form.Control type="text" placeholder="Address" required />
+            <Form.Control
+              type="text"
+              placeholder="Address"
+              name="address"
+              required
+              onChange={handleInput}
+            />
             <Form.Control.Feedback type="invalid">
               Please provide a valid Address.
             </Form.Control.Feedback>
@@ -66,14 +138,26 @@ const Checkout = () => {
         <Row className="mb-3">
           <Form.Group as={Col} md="6" controlId="validationCustom04">
             <Form.Label>City</Form.Label>
-            <Form.Control type="text" placeholder="City" required />
+            <Form.Control
+              type="text"
+              placeholder="City"
+              name="city"
+              required
+              onChange={handleInput}
+            />
             <Form.Control.Feedback type="invalid">
               Please provide a valid City.
             </Form.Control.Feedback>
           </Form.Group>
           <Form.Group as={Col} md="6" controlId="validationCustom05">
             <Form.Label>Contact Number</Form.Label>
-            <Form.Control type="text" placeholder="Contact Number" required />
+            <Form.Control
+              type="text"
+              placeholder="Contact Number"
+              name="contact"
+              onChange={handleInput}
+              required
+            />
             <Form.Control.Feedback type="invalid">
               Please provide a valid Contact Number.
             </Form.Control.Feedback>
@@ -83,14 +167,26 @@ const Checkout = () => {
         <Row className="mb-3">
           <Form.Group as={Col} md="6" controlId="validationCustom06">
             <Form.Label>Date to deliver</Form.Label>
-            <Form.Control type="date" placeholder="Date to deliver" required />
+            <Form.Control
+              type="date"
+              placeholder="Date to deliver"
+              name="deliverDate"
+              onChange={handleInput}
+              required
+            />
             <Form.Control.Feedback type="invalid">
               Please provide a valid date.
             </Form.Control.Feedback>
           </Form.Group>
           <Form.Group as={Col} md="6" controlId="validationCustom07">
             <Form.Label>Delivery Time</Form.Label>
-            <Form.Control type="text" placeholder="Time to Deliver" required />
+            <Form.Control
+              type="text"
+              placeholder="Time to Deliver"
+              name="deliverTime"
+              onChange={handleInput}
+              required
+            />
             <Form.Control.Feedback type="invalid">
               Please provide a valid Time.
             </Form.Control.Feedback>
@@ -99,14 +195,25 @@ const Checkout = () => {
         <Row className="mb-3">
           <Form.Group as={Col} md="12" controlId="validationCustom08">
             <Form.Label>Personal Message</Form.Label>
-            <Form.Control type="text" placeholder="Personal Message" />
+            <Form.Control
+              type="text"
+              placeholder="Personal Message"
+              name="message"
+              onChange={handleInput}
+            />
           </Form.Group>
         </Row>
         <h5>Sender's Information</h5>
         <Row className="mb-3">
           <Form.Group as={Col} md="12" controlId="validationCustom09">
             <Form.Label>Name</Form.Label>
-            <Form.Control required type="text" placeholder="Name" />
+            <Form.Control
+              required
+              type="text"
+              placeholder="Name"
+              name="senderName"
+              onChange={handleInput}
+            />
             <Form.Control.Feedback type="invalid">
               Please provide your Name.
             </Form.Control.Feedback>
@@ -115,14 +222,26 @@ const Checkout = () => {
         <Row className="mb-3">
           <Form.Group as={Col} md="6" controlId="validationCustom11">
             <Form.Label>Contact</Form.Label>
-            <Form.Control required type="text" placeholder="Contact" />
+            <Form.Control
+              required
+              type="text"
+              placeholder="Contact"
+              name="senderContact"
+              onChange={handleInput}
+            />
             <Form.Control.Feedback type="invalid">
               Please provide your Contact Number.
             </Form.Control.Feedback>
           </Form.Group>
           <Form.Group as={Col} md="6" controlId="validationCustom12">
             <Form.Label>Email</Form.Label>
-            <Form.Control required type="text" placeholder="Email" />
+            <Form.Control
+              required
+              type="text"
+              placeholder="Email"
+              name="senderEmail"
+              onChange={handleInput}
+            />
             <Form.Control.Feedback type="invalid">
               Please provide your Email.
             </Form.Control.Feedback>
