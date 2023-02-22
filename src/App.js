6401f3b42f878fcf03ebc64e03ec2644eb6a89e1
kchamithri <gangodaw-im18025@stu.kln.ac.lk>
2023-02-22 22:47:00 +0530

    protected routes added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AdminLogin from "./Components/Dashboard/AdminLogin";
 import AdminSideBar from "./Components/Dashboard/AdminSideBar";
 import Dashboard from "./Components/Dashboard/Dashboard";
+import AdminLogout from "./Components/Dashboard/AdminLogout";
 import Footer from "./Components/Website/Footer";
 import Navbar from "./Components/Website/Navbar";
 import Profile from "./Components/Website/Profile";
@@ -20,12 +22,49 @@ import Register from "./pages/Register";
 import About from "./pages/website/About";
 import CelebrationCake from "./pages/website/CelebrationCake";
 import Checkout from "./pages/website/checkout/Checkout";
+import Payment from "./pages/website/checkout/Payment";
 import DesignAcake from "./pages/website/DesignAcake";
 import Home from "./pages/website/Home";
 import PartyPacks from "./pages/website/PartyPacks";
+import ProtectedRoute from "./ProtectedRoute";
 import "./Styles/LandingPage.css";
+import UserLogout from "./Components/Website/UserLogout";
 
 function App() {
+  const [userauth, setUserauth] = useState(false);
+  const [adminauth, setAdminauth] = useState(false);
+
+  useEffect(() => {
+    console.log(adminauth);
+  }, [adminauth]);
+
+  // const isLogged = async () => {
+  //   try {
+  //     const res = await fetch("/auth", {
+  //       method: "GET",
+  //       headers: {
+  //         Accept: "application/json",
+  //         "Content-Type": "application/json",
+  //       },
+  //       credentials: "include",
+  //     });
+  //     if (res.status === 200) {
+  //       // setAdminauth(true)
+  //       setUserauth(true);
+  //     }
+  //     if (res.status === 401) {
+  //       setAdminauth(false);
+  //       setUserauth(false);
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   isLogged();
+  // }, []);
+
   return (
     <>
       {/* */}
@@ -37,14 +76,40 @@ function App() {
             <Route path="celebrationcakes" element={<CelebrationCake />} />
             <Route path="partypacks" element={<PartyPacks />} />
             <Route path="designacake" element={<DesignAcake />} />
-            <Route path="login" element={<Login />} />
+            <Route path="login" element={<Login setUserauth={setUserauth} />} />
             <Route path="register" element={<Register />} />
-            <Route path="cart" element={<Cart />} />
+            <Route
+              path="userLogout"
+              element={<UserLogout setUserauth={setUserauth} />}
+            />
+
+            <Route
+              path="cart"
+              element={
+                <ProtectedRoute userauth={userauth}>
+                  <Cart />
+                </ProtectedRoute>
+              }
+            />
             <Route path="checkout" element={<Checkout />} />
-            <Route path="profile" element={<Profile />} />
+            <Route
+              path="profile"
+              element={
+                <ProtectedRoute userauth={userauth}>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
           </Route>
 
-          <Route path="/admin" element={<AdminSideBar />}>
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute adminauth={adminauth}>
+                <AdminSideBar />
+              </ProtectedRoute>
+            }
+          >
             <Route index={true} element={<Dashboard />} />
             <Route path="reports" element={<Reports />} />
             <Route path="products" element={<AddProducts />} />
@@ -52,10 +117,17 @@ function App() {
             <Route path="orders" element={<Orders />} />
             <Route path="inventory" element={<Inventory />} />
             <Route path="settings" element={<Settings />} />
+            <Route
+              path="adminLogout"
+              element={<AdminLogout setAdminauth={setAdminauth} />}
+            />
           </Route>
 
-          <Route path="/admin/login" element={<AdminLogin />}></Route>
-          <Route path="/adminSidebar" element={<AdminSideBar />}></Route>
+          <Route
+            path="/admin/login"
+            element={<AdminLogin setAdminauth={setAdminauth} />}
+          ></Route>
+          {/* <Route path="/adminSidebar" element={<AdminSideBar />}></Route> */}
 
           {/* <Route path="/dashboard" element={<Dashboard />} /> */}
         </Routes>
